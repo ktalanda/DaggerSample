37e1ecf39d840101c9a5cfe030b127dda9cbb364
k.talanda <kamil0916@gmail.com>
2016-06-03 08:54:34 +0200

    clean up

--- app/src/main/java/example/com/daggersample/MainApplication.java
+++ app/src/main/java/example/com/daggersample/MainApplication.java
@@ -1,20 +1,7 @@
 package example.com.daggersample;
 
 import android.app.Application;
-import android.content.Context;
-
-import timber.log.Timber;
 
 public class MainApplication extends Application {
-
-    public static MainApplication get(Context context) {
-        return (MainApplication) context.getApplicationContext();
-    }
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        Timber.plant(new Timber.DebugTree());
-    }
 
 }
